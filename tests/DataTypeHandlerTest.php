9593e01b42ebd5bfbcdec7c957e4488a47a6a4ce
marijoo <marijan@kolossal.io>
2023-08-15 11:31:16 +0200

    chore: update packages :arrow_up:

--- tests/DataTypeHandlerTest.php
+++ tests/DataTypeHandlerTest.php
@@ -19,7 +19,7 @@ use stdClass;
  */
 class DataTypeHandlerTest extends TestCase
 {
-    public function handlerProvider()
+    public static function handlerProvider()
     {
         $timestamp = '2017-01-01 00:00:00.000000+0000';
         $datetime = Carbon::createFromFormat('Y-m-d H:i:s.uO', $timestamp);
