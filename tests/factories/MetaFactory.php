c192cd0c9964297d3966746ea2818b78497354f2
marijoo <marijan@kolossal.io>
2026-04-18 15:55:56 +0200

    test: use random values in meta factory

--- tests/factories/MetaFactory.php
+++ tests/factories/MetaFactory.php
@@ -9,11 +9,22 @@ class MetaFactory extends Factory
 {
     protected $model = Meta::class;
 
+    protected function getValue(): mixed
+    {
+        return $this->faker->randomElement([
+            $this->faker->word(),
+            $this->faker->numberBetween(),
+            $this->faker->boolean(),
+            $this->faker->dateTime(),
+            null,
+        ]);
+    }
+
     public function definition()
     {
         return [
             'key' => $this->faker->domainWord(),
-            'value' => $this->faker->randomNumber(5),
+            'value' => $this->getValue(),
         ];
     }
 }
